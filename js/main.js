00b4de6e5864df569cf049017bc6040ee162e6e1
Alexis Carbajal <2020pomelo@gmail.com>
2025-01-31 19:10:20 -0600

    Pagina principal acabada

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-window.addEventListener('load', function() {
-    // Cuando la página termine de cargar, aseguramos que el scroll esté en la parte superior
-    window.scrollTo(0, 0);
-  });
+// window.addEventListener('load', function() {
+
+//     window.scrollTo(0, 0);
+//   });
   
-window.onload = () => {
-    document.body.style.overflow = 'hidden';
+// window.onload = () => {
+//     document.body.style.overflow = 'hidden';
 
-    const totalAnimationTime = 7 * 1000;
+//     const totalAnimationTime = 7 * 1000;
 
-    setTimeout(() => {
-      document.body.style.overflow = 'auto';
-    }, totalAnimationTime);
-  };
+//     setTimeout(() => {
+//       document.body.style.overflow = 'auto';
+//     }, totalAnimationTime);
+//   };
   
